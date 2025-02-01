e5af38acc432cf9f9c93b2f01ab275142512bfb7
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2025-02-01 12:29:59 +0700

    refactor(route): making the route cleaner

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -3,8 +3,6 @@ package routes
 import (
 	"raion-assessment/internal/di"
 
-	_ "raion-assessment/docs"
-
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -12,5 +10,5 @@ func SetupRoutes(app *fiber.App, container di.Container, jwtSecret string) {
 	setupStaticRoutes(app)
 	setupDocsRoutes(app)
 	setupAPIRoutes(app, container, jwtSecret)
-	setupNotFoundHandler(app)
+	setupErrorRoutes(app)
 }
